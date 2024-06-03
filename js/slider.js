1461122b2a1b1631fd6ef252701b949162098b1f
Daria Savelyeva <ssavellievaa@yandex.ru>
2024-06-03 17:11:06 +0400

    верстка секции suggestion

--- js/slider.js
+++ js/slider.js
@@ -12,10 +12,37 @@ $(function () {
 		$('.gallery__slider').slick('slickNext')
 	})
 
+	// second slider
 	$('.collection__slider').slick({
 		arrows: false,
 		slidesToShow: 3,
 		infinite: true,
+		appendArrows: $('.collection__arrow'),
 	})
+	$('.collection__prev').on('click', function (e) {
+		e.preventDefault()
+		$('.collection__slider').slick('slickPrev')
+	})
+	$('.collection__next').on('click', function (e) {
+		e.preventDefault()
+		$('.collection__slider').slick('slickNext')
+	})
+
+	// third slider
+	$('.suggestion__slider').slick({
+		arrows: false,
+		slidesToShow: 5,
+		infinite: true,
+		appendArrows: $('.collection__arrow'),
+	})
+	$('.suggestion__prev').on('click', function (e) {
+		e.preventDefault()
+		$('.suggestion__slider').slick('slickPrev')
+	})
+	$('.suggestion__next').on('click', function (e) {
+		e.preventDefault()
+		$('.suggestion__slider').slick('slickNext')
+	})
+
 })
 
