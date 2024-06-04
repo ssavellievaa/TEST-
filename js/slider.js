69e81a1b0b4ba4c46cb4631b773462900c40d038
Daria Savelyeva <ssavellievaa@yandex.ru>
2024-06-04 12:50:12 +0400

    медиазапросы и реализация бургер-меню

--- js/slider.js
+++ js/slider.js
@@ -5,7 +5,27 @@ $(function () {
 		arrows: false,
 		slidesToShow: 4.2,
 		infinite: false,
-		appendArrows: $('.gallery__slider-arrows'),
+		responsive: 
+		[
+			{
+				breakpoint: 993,
+				settings: {
+					slidesToShow: 3.2,
+				},
+			},
+			{
+				breakpoint: 798,
+				settings: {
+					slidesToShow: 2.2,
+				},
+			},
+			{
+				breakpoint: 591,
+				settings: {
+					slidesToShow: 1,
+				},
+			},
+		]
 	})
 	$('.gallery__slider-next').on('click', function (e) {
 		e.preventDefault()
@@ -18,6 +38,33 @@ $(function () {
 		slidesToShow: 3,
 		infinite: true,
 		appendArrows: $('.collection__arrow'),
+		responsive: 
+		[
+			{
+				breakpoint: 1281,
+				settings: {
+					slidesToShow: 2,
+				},
+			},
+			{
+				breakpoint: 993,
+				settings: {
+					slidesToShow: 1,
+				},
+			},
+			{
+				breakpoint: 798,
+				settings: {
+					slidesToShow: 2,
+				},
+			},
+			{
+				breakpoint: 591,
+				settings: {
+					slidesToShow: 1,
+				},
+			},
+		]
 	})
 	$('.collection__prev').on('click', function (e) {
 		e.preventDefault()
@@ -34,6 +81,33 @@ $(function () {
 		slidesToShow: 5,
 		infinite: true,
 		appendArrows: $('.collection__arrow'),
+		responsive: 
+		[
+			{
+				breakpoint: 1281,
+				settings: {
+					slidesToShow: 4,
+				},
+			},
+			{
+				breakpoint: 993,
+				settings: {
+					slidesToShow: 3,
+				},
+			},
+			{
+				breakpoint: 798,
+				settings: {
+					slidesToShow: 2,
+				},
+			},
+			{
+				breakpoint: 591,
+				settings: {
+					slidesToShow: 1,
+				},
+			},
+		]
 	})
 	$('.suggestion__prev').on('click', function (e) {
 		e.preventDefault()
@@ -42,6 +116,12 @@ $(function () {
 	$('.suggestion__next').on('click', function (e) {
 		e.preventDefault()
 		$('.suggestion__slider').slick('slickNext')
+	})
+
+	// burger
+	$('.burger').on('click', function (e) {
+		e.preventDefault()
+		$('.header__top').toggleClass('header__top--open')
 	})
 
 })
